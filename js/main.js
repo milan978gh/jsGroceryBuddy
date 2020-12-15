e61acc4eab4eb5c9ac89b065b57e7c4e3a158819
milan978gh <amilandev78@gmail.com>
2020-12-15 17:43:09 +0100

    update

--- js/main.js
+++ js/main.js
@@ -7,11 +7,36 @@ const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-btn');
 // edit option
+let editElement;
+let editFlag = false;
+let editId = '';
 
 // ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  // duzi oblik
+  // if (value !== '' && editFlag === false) {
+  // } else if (value !== '' && editFlag === true) {
+  // } else {}
+  // kraci oblik
+  if (value && !editFlag) {
 
+  } else if (value && editFlag) {
+
+  } else {
+
+  }
+}
+// display alert
+function displayAlert() {
+  alert.textContent = 'bla';
+  alert.classList.add('alert-danger');
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
