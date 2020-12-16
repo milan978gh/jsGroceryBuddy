00de3db54adb8ecf0d2b9d0a9fe3cb856a442514
milan978gh <amilandev78@gmail.com>
2020-12-16 12:33:08 +0100

    update

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ let editId = '';
 
 // ****** EVENT LISTENERS **********
 form.addEventListener('submit', addItem);
+clearBtn.addEventListener('click', clearItems);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
@@ -25,18 +26,70 @@ function addItem(e) {
   // } else {}
   // kraci oblik
   if (value && !editFlag) {
-
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<div class="btn-container">
+          <button type="button" class="check-btn">
+            <img src="./img/checkBox-24.png">
+          </button>
+        </div>
+        <p class="title">${value}</p>
+        <div class="btn-container">
+          <button type="button" class="edit-btn">
+            <img src="./img/edit-24.png">
+          </button>
+          <button type="button" class="delete-btn">
+            <img src="./img/delete-24.png">
+          </button>
+        </div>`;
+    list.appendChild(element);
+    displayAlert('item added', 'success');
+    container.classList.add('show-container');
+    // add to local storage
+    addToLocalStorage(id, value);
+    // set back to default
+    setBackToDefault();
   } else if (value && editFlag) {
 
   } else {
-
+    displayAlert(`please enter value`, 'danger');
   }
 }
 // display alert
-function displayAlert() {
-  alert.textContent = 'bla';
-  alert.classList.add('alert-danger');
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  // remove alert
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+function clearItems() {
+  const items = document.querySelectorAll('.grocery-item');
+  if (items.length > 0) {
+    items.forEach((item) => {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove('show-container');
+  displayAlert('empty list', 'danger');
+  setBackToDefault();
+  // localStorage.removeItem('list');
+}
+
+function setBackToDefault() {
+  grocery.value = '';
+  editFlag = false;
+  editId = '';
+  submitBtn.textContent = 'submit';
 }
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log('b');
+}
 // ****** SETUP ITEMS **********
